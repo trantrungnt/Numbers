c8d0e7456b572609061e88a0bb9d85d4ced6b20b
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-12 05:42:13 -0800

    type input Numbers

--- Numbers/src/numbers/Numbers.java
+++ Numbers/src/numbers/Numbers.java
@@ -5,6 +5,9 @@
  */
 package numbers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author TrungNT
@@ -15,7 +18,23 @@ public class Numbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int inputA, inputB;
+        
+        try
+        {
+            
+            BufferedReader oBufferReader = new BufferedReader(new InputStreamReader(System.in));
+            
+            System.out.print("\nNhap so thu nhat a=");
+            inputA = Integer.parseInt(oBufferReader.readLine());
+            
+            System.out.print("\nNhap so thu nhat b=");
+            inputB = Integer.parseInt(oBufferReader.readLine());
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
     }
     
 }
